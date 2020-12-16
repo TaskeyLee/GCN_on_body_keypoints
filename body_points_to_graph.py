3a0094696bf9371e2c6b799c8272346d7124454b
Taskey <1524071136@qq.com>
2020-12-16 11:12:39 +0800

    将关键点检测功能封装为函数，方便使用

--- body_points_to_graph.py
+++ body_points_to_graph.py
@@ -15,50 +15,51 @@ import matplotlib.pyplot as plt
 torch.manual_seed(100)
 
 # 导入实验小型数据集（站立&坐下）
-files_1 = os.listdir('C:/Users/lab/Desktop/taskey/action_video_test/sit')
-files_2 = os.listdir('C:/Users/lab/Desktop/taskey/action_video_test/stand')
+img_filenames_sit = 'C:/Users/lab/Desktop/taskey/action_video_test/sit/'
+img_filenames_stand = 'C:/Users/lab/Desktop/taskey/action_video_test/stand/'
+files_1 = os.listdir(img_filenames_sit)
+files_2 = os.listdir(img_filenames_stand)
 
 # graph中src和dst节点(人体关键点指向关系)
 src = np.array([17, 15, 18, 16, 0, 2, 3, 4, 5, 6, 7, 8, 9, 12, 10, 13, 11, 14, 23, 22, 24, 20, 19, 21])
 dst = np.array([15, 0, 16, 0, 1, 1, 2, 3, 1, 5, 6, 1, 8, 8, 9, 12, 10, 13, 22, 11, 11, 19, 14, 14])
 
-# 检测人体关键点
-bodies_points = []
-for img in files_1:
-    body_point = get_body_points('C:/Users/lab/Desktop/taskey/action_video_test/sit/' + img)
-    bodies_points.append(body_point)
-    
-body_graph = [] # 空列表用于存储graph
+# 用于根据图片检测人体关键点
+def detect_body_point_and_save(img_filenames, imgs, save_name, label):
+    # 若已存在对应的.npy，则直接导入，不再重新检测关键点
+    if os.path.exists(save_name):
+        print('Body Point data have already existed')
+        bodies_points = np.load(save_name)
+        
+    else:
+        # 若无对应.npy，则基于openpose进行人体关键点检测
+        bodies_points = []
+        for img in imgs:
+            # print(img_filenames + img)
+            body_point = get_body_points(img_filenames + img)
+            bodies_points.append(body_point)
+            bodies_points_array = np.array(bodies_points)
+            np.save(save_name, bodies_points_array)
+        
+    body_graph = [] # 空列表用于存储graph
+    for body_points in bodies_points: # 遍历所有检测图片结果
+        for body_point in body_points: # 由于某些图片中存在多个人，所以再次再同一张图片结果中遍历所有人体
+            g = dgl.DGLGraph((src, dst)) # 建立graph
+            g = dgl.add_self_loop(g)
+            plt.figure()
+            pos = body_point[:,0:2] # 取前两列坐标（第三列为置信度），作为显示时的节点坐标
+            pos[:, 1] = -pos[:, 1]  # 由于默认显示坐标原点再左上角，鉴于人眼观察习惯，将其坐标上下翻转
+            nx.draw(g.to_networkx(), pos = pos, with_labels=True) # 显示graph
+            node_feature = body_point
+            g.ndata['coordinate'] = torch.tensor(node_feature)
+            body_graph.append([g, torch.tensor(label)])
+    return body_graph
 
-
-for body_points in bodies_points: # 遍历所有检测图片结果
-    for body_point in body_points: # 由于某些图片中存在多个人，所以再次再同一张图片结果中遍历所有人体
-        g = dgl.DGLGraph((src, dst)) # 建立graph
-        g = dgl.add_self_loop(g)
-        plt.figure()
-        pos = body_point[:,0:2] # 取前两列坐标（第三列为置信度），作为显示时的节点坐标
-        pos[:, 1] = -pos[:, 1]  # 由于默认显示坐标原点再左上角，鉴于人眼观察习惯，将其坐标上下翻转
-        nx.draw(g.to_networkx(), pos = pos, with_labels=True) # 显示graph
-        node_feature = body_point
-        g.ndata['coordinate'] = torch.tensor(node_feature)
-        body_graph.append([g, torch.tensor(0)])
-
-bodies_points = []
-for img in files_2:
-    body_point = get_body_points('C:/Users/lab/Desktop/taskey/action_video_test/stand/' + img)
-    bodies_points.append(body_point)
-    
-for body_points in bodies_points: # 遍历所有检测图片结果
-    for body_point in body_points: # 由于某些图片中存在多个人，所以再次再同一张图片结果中遍历所有人体
-        g = dgl.DGLGraph((src, dst)) # 建立graph
-        g = dgl.add_self_loop(g)
-        plt.figure()
-        pos = body_point[:,0:2] # 取前两列坐标（第三列为置信度），作为显示时的节点坐标
-        pos[:, 1] = -pos[:, 1]  # 由于默认显示坐标原点再左上角，鉴于人眼观察习惯，将其坐标上下翻转
-        nx.draw(g.to_networkx(), pos = pos, with_labels=True) # 显示graph
-        node_feature = body_point / 100
-        g.ndata['coordinate'] = torch.tensor(node_feature)
-        body_graph.append([g, torch.tensor(1)])
+# 分别生成坐姿与站姿的graph及其label
+body_graph_sit = detect_body_point_and_save(img_filenames_sit, files_1, 'sit.npy', 0)
+body_graph_stand = detect_body_point_and_save(img_filenames_stand, files_2, 'stand.npy', 1)
+# 合并为一个list
+body_graph = body_graph_sit + body_graph_stand
 
 # 构建pytorch所使用的dataloader
 
@@ -72,13 +73,8 @@ dataloader = DataLoader(
     body_graph,
     collate_fn=collate,
     shuffle = True)
-        
-    
-#         # graph可视化
-#         pos = node_feature[:,0:2] # 取前两列坐标（第三列为置信度），作为显示时的节点坐标
-#         pos[:, 1] = -pos[:, 1]  # 由于默认显示坐标原点再左上角，鉴于人眼观察习惯，将其坐标上下翻转
-#         nx.draw(g.to_networkx(), pos = pos, with_labels=True) # 显示graph
 
+# 搭建GCN模型
 class GCN(nn.Module):
     def __init__(self, in_dim, hidden_dim, n_class):
         super(GCN, self).__init__()
@@ -99,7 +95,7 @@ class GCN(nn.Module):
 src = np.array([17, 15, 18, 16, 0, 2, 3, 4, 5, 6, 7, 8, 9, 12, 10, 13, 11, 14, 23, 22, 24, 20, 19, 21])
 dst = np.array([15, 0, 16, 0, 1, 1, 2, 3, 1, 5, 6, 1, 8, 8, 9, 12, 10, 13, 22, 11, 11, 19, 14, 14])
 graph = dgl.DGLGraph((src, dst)) # 建立graph
-graph = dgl.add_self_loop(g)
+graph = dgl.add_self_loop(graph)
  
 model = GCN(3, 10, 2)                
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.000000001)
